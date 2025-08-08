4f75d92e2f9069cf2f6736eddf02b3859c0b08b8
fauzil17 <mutaqqimfauzil@gmail.com>
2025-08-08 09:43:07 +0700

    Menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coaba git remote 2 dari local ke github</title>
+</head>
+<body>
+    <h1>Hello Word !</h1>
+</body>
+</html>
